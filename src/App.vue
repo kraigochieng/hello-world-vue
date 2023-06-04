4cff6ec74ceef570d76912b2f9d2b71c536eaf9b
Kraig Ochieng <kraigochieng@gmail.com>
2023-06-04 12:36:31 +0300

    Finished Form Handling and Events

--- src/App.vue
+++ src/App.vue
@@ -66,8 +66,66 @@
     <button @click="decrement(1, $event)">Decrement</button>
     <button @click="increment(2)">Increment 2</button>
     <button @click="decrement(2, $event)">Decrement 2</button>
-    <!--We have to inject-->
+    <!--We have to inject object-->
     <button @click="changeName($event), decrement(5, $event)">Change Name and Decrement 5</button>
+
+    <!--Forms-->
+    <h1>Forms</h1>
+    <form @submit="submitForm">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="form.name">
+      </div>
+      <div>
+        <label for="profile">Profile Summary</label>
+        <textarea id="profile" v-model="form.profile_summary"></textarea>
+      </div>
+      <div>
+        <label for="country">Country</label>
+        <select id="country" v-model="form.country">
+          <option selected disabled value="">Select A country</option>
+          <option value="kenya">Kenya</option>
+          <option value="uganda">Uganda</option>
+          <option value="tanzania">Tanzania</option>
+          <option value="burundi">Burundi</option>
+        </select>
+      </div>
+      <div>
+        <label for="job-location">Job Location</label>
+        <select id="job-location" multiple v-model="form.job_location">
+          <option value="kenya">Kenya</option>
+          <option value="uganda">Uganda</option>
+          <option value="tanzania">Tanzania</option>
+          <option value="burundi">Burundi</option>
+        </select>
+      </div>
+
+      <div>
+        <input type="checkbox" id="remote-work" v-model="form.remote_work" true-value="yes" false-value="no">
+        <label for="remote-work">Remote Work?</label>
+      </div>
+
+      <div>
+        <label>Skill Set</label>
+          <input type="checkbox" id="html" name="skillset" value="html" v-model="form.skillset">
+          <label for="html">HTML</label>
+          <input type="checkbox" id="css" name="skillset" value="css" v-model="form.skillset">
+          <label for="css">CSS</label>
+      </div>
+
+      <div>
+        <input type="radio" name="experience" id="0-2" value="0-2" v-model="form.experience">
+        <label for="0-2">0-2</label>
+        <input type="radio" name="experience" id="2-4" value="2-4" v-model="form.experience">
+        <label for="2-4">2-4</label>
+        <input type="radio" name="experience" id="4-8" value="4-8" v-model="form.experience">
+        <label for="4-6">4-6</label>
+        <input type="radio" name="experience" id="8+" value="8+" v-model="form.experience">
+        <label for="8+">8+</label>
+      </div>
+      <p>{{ JSON.stringify(form, null, 2) }}</p>
+      <button type="submit">Submit</button>
+    </form>
 </div>
 </template>
 
@@ -112,6 +170,15 @@ export default {
 
       count: 0,
 
+      form: {
+        name: '',
+        profile_summary: '',
+        country: '',
+        job_location: [],
+        remote_work: 'no',
+        skillset: [],
+        experience: '',
+      },
     }
   },
     
@@ -142,6 +209,10 @@ export default {
       console.log('Event: ', event)
     },
 
+    submitForm(event) {
+      event.preventDefault()
+      console.log(this.form)
+    }
   }
 }
 
